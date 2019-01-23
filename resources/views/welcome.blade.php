c6a79eef247b4e7f7d37ac018d399892ce5f1a49
dewisekar <sekar.6499@gmail.com>
2019-01-23 20:38:27 +0700

    initial commit - template front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,207 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        IFC 2019
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <!-- Nucleo Icons -->
+    <link href="./blk/css/nucleo-icons.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="./blk/css/blk-design-system.css?v=1.0.0" rel="stylesheet" />
+</head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<body class="index-page">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
+        <div class="container">
+            <div class="navbar-translate">
+              <a class="navbar-brand" href="https://demos.creative-tim.com/blk-design-system/index.html" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+                <span>BLK•</span> Design System
+              </a>
+              <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                <div class="navbar-collapse-header">
+                    <div class="row">
+                        <div class="col-6 collapse-brand">
+                            <a>
+                                BLK•
+                            </a>
+                        </div>
+                        <div class="col-6 collapse-close text-right">
+                            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                                <i class="tim-icons icon-simple-remove"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item p-0">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
+                            <i class="fab fa-twitter"></i>
+                            <p class="d-lg-none d-xl-none">Twitter</p>
+                        </a>
+                    </li>
+                    <li class="nav-item p-0">
+                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+                            <i class="fab fa-facebook-square"></i>
+                            <p class="d-lg-none d-xl-none">Facebook</p>
+                        </a>
+                    </li>
+                    <li class="nav-item p-0">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+                            <i class="fab fa-instagram"></i>
+                            <p class="d-lg-none d-xl-none">Instagram</p>
+                        </a>
+                    </li>
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                            <i class="fa fa-cogs d-lg-none d-xl-none"></i> Getting started
+                        </a>
+                        <div class="dropdown-menu dropdown-with-icons">
+                            <a href="https://demos.creative-tim.com/blk-design-system/docs/1.0/getting-started/overview.html" class="dropdown-item">
+                                <i class="tim-icons icon-paper"></i> Documentation
+                            </a>
+                            <a href="examples/register-page.html" class="dropdown-item">
+                                <i class="tim-icons icon-bullet-list-67"></i>Register Page
+                            </a>
+                            <a href="examples/landing-page.html" class="dropdown-item">
+                                <i class="tim-icons icon-image-02"></i>Landing Page
+                            </a>
+                            <a href="examples/profile-page.html" class="dropdown-item">
+                                <i class="tim-icons icon-single-02"></i>Profile Page
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-default d-none d-lg-block" href="javascript:void(0)" onclick="scrollToDownload()">
+                            <i class="tim-icons icon-cloud-download-93"></i> Download
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+    <div class="wrapper">
+        <!--Kotak kotak landing page-->
+        <div class="page-header header-filter">
+            <div class="squares square1"></div>
+            <div class="squares square2"></div>
+            <div class="squares square3"></div>
+            <div class="squares square4"></div>
+            <div class="squares square5"></div>
+            <div class="squares square6"></div>
+            <div class="squares square7"></div>
+            <div class="container">
+                <div class="content-center brand">
+                    <h1 class="h1-seo">BLK•</h1>
+                    <h3>A beautiful Design System for Bootstrap 4. It's Free and Open Source.</h3>
                 </div>
             </div>
         </div>
-    </body>
+        <div class="main">
+
+        </div>
+        <footer class="footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3">
+                <h1 class="title">BLK•</h1>
+              </div>
+              <div class="col-md-3">
+                <ul class="nav">
+                  <li class="nav-item">
+                    <a href="./index.html" class="nav-link">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./examples/landing-page.html" class="nav-link">
+                      Landing
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./examples/register-page.html" class="nav-link">
+                      Register
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./examples/profile-page.html" class="nav-link">
+                      Profile
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <ul class="nav">
+                  <li class="nav-item">
+                    <a href="https://creative-tim.com/contact-us" class="nav-link">
+                      Contact Us
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://creative-tim.com/about-us" class="nav-link">
+                      About Us
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://creative-tim.com/blog" class="nav-link">
+                      Blog
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://opensource.org/licenses/MIT" class="nav-link">
+                      License
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h3 class="title">Follow us:</h3>
+                <div class="btn-wrapper profile">
+                  <a target="_blank" href="https://twitter.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                  <a target="_blank" href="https://www.facebook.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
+                    <i class="fab fa-facebook-square"></i>
+                  </a>
+                  <a target="_blank" href="https://dribbble.com/creativetim" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+                    <i class="fab fa-dribbble"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+    </div>
+    <!--   Core JS Files   -->
+    <script src="./blk/js/core/jquery.min.js" type="text/javascript"></script>
+    <script src="./blk/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="./blk/js/core/bootstrap.min.js" type="text/javascript"></script>
+    <script src="./blk/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+    <script src="./blk/js/plugins/bootstrap-switch.js"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="./blk/js/plugins/nouislider.min.js" type="text/javascript"></script>
+    <!-- Chart JS -->
+    <script src="./blk/js/plugins/chartjs.min.js"></script>
+    <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+    <script src="./blk/js/plugins/moment.min.js"></script>
+    <script src="./blk/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+    <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
+    <script src="./blk/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+</body>
+
 </html>
