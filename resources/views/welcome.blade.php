da2ffb0b0a7efd026455c4c01762a67805912730
dewisekar <sekar.6499@gmail.com>
2019-01-24 23:02:54 +0700

    progress buttons + smoothscroll + animate on scroll

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,31 +5,36 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel=" icon" href="../img/logoIFC2.png" type="image/png">
     <title>
-        IFC 2019
+      IFC 2019
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <!-- Nucleo Icons -->
     <link href="./blk/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="./blk/css/addition.css" rel="stylesheet" />
     <!-- CSS Files -->
     <link href="./blk/css/blk-design-system.css?v=1.0.0" rel="stylesheet" />
+    <!--aos-->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
 <body class="index-page">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
+    <nav class="navbar navbar-expand-lg fixed-top  navbar-transparent">
         <div class="container">
-            <div class="navbar-translate">
-              <a class="navbar-brand" href="https://demos.creative-tim.com/blk-design-system/index.html" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
-                <span>BLK•</span> Design System
-              </a>
-              <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
+            <div class="navbar-translate">                
+                <a class="navbar-brand" href="../">
+                    <img src="../img/logoIFC.png" width="20%">
+                </a>
+                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-bar bar1"></span>
+                  <span class="navbar-toggler-bar bar2"></span>
+                  <span class="navbar-toggler-bar bar3"></span>
+                </button>
             </div>
             <div class="collapse navbar-collapse justify-content-end" id="navigation">
                 <div class="navbar-collapse-header">
@@ -47,47 +52,28 @@
                     </div>
                 </div>
                 <ul class="navbar-nav">
+                    <style>
+                      #navmenu2:hover  {
+                        background-color:rgba(69, 199, 188,0.7);
+                        transition: 0.5s;
+                      }
+                      #navmenu2{
+                        border-radius:5px;
+                      }
+                    </style>
                     <li class="nav-item p-0">
-                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
-                            <i class="fab fa-twitter"></i>
-                            <p class="d-lg-none d-xl-none">Twitter</p>
-                        </a>
+                      <a class="nav-link" id="navmenu" href="">Pendaftaran</a>
                     </li>
                     <li class="nav-item p-0">
-                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
-                            <i class="fab fa-facebook-square"></i>
-                            <p class="d-lg-none d-xl-none">Facebook</p>
-                        </a>
+                      <a class="nav-link" id="navmenu" href="">Timeline</a>
                     </li>
                     <li class="nav-item p-0">
-                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
-                            <i class="fab fa-instagram"></i>
-                            <p class="d-lg-none d-xl-none">Instagram</p>
-                        </a>
+                      <a class="nav-link" id="navmenu" href="#"  rel="tooltip" title="Coming Soon!" data-placement="bottom">Jadwal Pertandingan</a>
                     </li>
-                    <li class="dropdown nav-item">
-                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                            <i class="fa fa-cogs d-lg-none d-xl-none"></i> Getting started
-                        </a>
-                        <div class="dropdown-menu dropdown-with-icons">
-                            <a href="https://demos.creative-tim.com/blk-design-system/docs/1.0/getting-started/overview.html" class="dropdown-item">
-                                <i class="tim-icons icon-paper"></i> Documentation
-                            </a>
-                            <a href="examples/register-page.html" class="dropdown-item">
-                                <i class="tim-icons icon-bullet-list-67"></i>Register Page
-                            </a>
-                            <a href="examples/landing-page.html" class="dropdown-item">
-                                <i class="tim-icons icon-image-02"></i>Landing Page
-                            </a>
-                            <a href="examples/profile-page.html" class="dropdown-item">
-                                <i class="tim-icons icon-single-02"></i>Profile Page
-                            </a>
+                    <li class="nav-item p-0">
+                        <div class="pl-2 pr-2" id="navmenu2" style="border: 2px solid white; color: white; display:inline-block;">
+                            <a class="nav-link" id="navmenu" href="../login" >Login</a>
                         </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-default d-none d-lg-block" href="javascript:void(0)" onclick="scrollToDownload()">
-                            <i class="tim-icons icon-cloud-download-93"></i> Download
-                        </a>
                     </li>
                 </ul>
             </div>
@@ -104,15 +90,106 @@
             <div class="squares square5"></div>
             <div class="squares square6"></div>
             <div class="squares square7"></div>
-            <div class="container">
+            <div class="container" data-aos="fade-up">
                 <div class="content-center brand">
-                    <h1 class="h1-seo">BLK•</h1>
-                    <h3>A beautiful Design System for Bootstrap 4. It's Free and Open Source.</h3>
+                    <div class="pt-3 pb-3 mt-0 mb-1" style="background-color:  rgba(69, 199, 188,0.5); border-radius:100px; border:none;">
+                        <img src="../img/logoIFC.png" class="mt-0" style="width:90%; height:auto;">
+                    </div>                    
+                    <h3 class="mb-3" style="font-family: 'Roboto', sans-serif;">TAKE IT TO THE NEXT LEVEL!</h3>
+                    <style>
+                      #btn1{
+                        background-image: url("../img/btn1.png");
+                        background-position: center;
+                        color: black;
+                        border: 2px solid white;
+                        font-size: 18px;
+                        font-weight: 600;
+                        transition: 1s;
+                      }
+                      #btn1:hover{
+                        color:white;
+                        background-color: transparent;
+                      }
+                    </style>
+                    <button class="btn mt-0" id="btn1" onclick="window.location.href='#sejarah'" type="button">EXPLORE!</button>
                 </div>
             </div>
         </div>
         <div class="main">
-
+          <div class="section">
+            <div class="container" id="sejarah">
+                <div class="title">
+                  <style>
+                    @media screen and (max-width: 992px) {
+                      #logoukm {
+                        width:60%;
+                      }
+                    }
+                  </style>
+                  <img src="../img/logoUKM.png" width="20%" id="logoukm" data-aos="fade-right">
+                  <h3 data-aos="fade-right">UKM Sepakbola ITS Presents:</h3>
+                </div>
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-lg-5 mb-5 mb-lg-0 " data-aos="fade-right">
+                        <h1 class="text-white font-weight-light">ITS Futsal Championship 2019</h1>
+                        <p class="text-white mt-4">IFC merupakan salah satu event dari UKM Sepakbola ITS dan kompetisi futsal antar departemen di ITS. IFC diikuti oleh 32 tim putra dan 16 tim putri dari departemen-departemen di iTS pada tahun 2017 dan 2018. Tahun 2019 merupakan tahun kesepuluh IFC diadakan. Jangan lupa untuk siapkan tim terbaikmu agar tidak ketinggalan event futsal terbesar tahunan di ITS!</p>
+                        
+                    </div>
+                    <div class="col-lg-6" data-aos="fade-left">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="../img/carousel/c1.jpg" alt="First slide">
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <p>Pertandingan final putra antara T. Material VS T. Sistem Perkapalan</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="../img/carousel/c2.jpg" alt="First slide">
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <p>Pertandingan final putri antara Kimia VS T. Geomatika</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item ">
+                                    <img class="d-block w-100" src="../img/carousel/c3.jpg" alt="First slide">
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <p style="color:black;">Pertandingan persahabatan antara UKM Sepakbola ITS VS Timnas Futsal Indonesia</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item ">
+                                    <img class="d-block w-100" src="../img/carousel/c4.jpg" alt="First slide">
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <p style="color:black;">Champion Tim Putri</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item ">
+                                    <img class="d-block w-100" src="../img/carousel/c5.jpg" alt="First slide">
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <p>Champion Tim Putra</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <i class="tim-icons icon-minimal-left"></i>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <i class="tim-icons icon-minimal-right"></i>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                        <p class="text-center">Dokumentasi IFC 2018</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="section">
+            <div class="container" id="persyaratan">
+              <div class="row">
+                
+              </div>
+            </div>
+          </div>
         </div>
         <footer class="footer">
           <div class="container">
@@ -202,6 +279,11 @@
     <script src="./blk/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
     <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
     <script src="./blk/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+    <!--aos-->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
 </body>
 
 </html>
